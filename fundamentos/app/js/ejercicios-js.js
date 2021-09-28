8c40ccb00f92bda51082396a624de5d922c3211a
blmanu <banderalazaromanuel@gmail.com>
2021-09-28 16:56:07 +0200

    Ejercicios todos sin TDD

--- fundamentos/app/js/ejercicios-js.js
+++ fundamentos/app/js/ejercicios-js.js
@@ -42,4 +42,85 @@ function juego(){
     }
 }
 
-juego();
+//juego();
+
+
+//Ejercicio 3
+
+function inicializarArray(longitud, valor){
+    if(typeof longitud !== 'number') return 'ERROR de longitud';
+    let array = new Array(longitud);
+    for(let i=0;i<array.length;i++)
+        array[i]=valor;
+    return array;
+}
+
+console.log(inicializarArray(4,""));
+
+//Ejercicio 4
+
+function esPrimo(num){
+	if (num == 0 || num == 1) return false;
+	for (let i = 2; i < num; i++) {
+		if (num % i == 0) return false;
+	}
+	return true;
+}
+
+function numerosPrimosEntreEstosNumeros(a,b){
+    let arrayPrimos = new Array();
+    for(let i = a; i < b; i++){
+        if(esPrimo(i))
+            arrayPrimos.push(i);
+    }
+    return arrayPrimos;
+}
+
+console.log(numerosPrimosEntreEstosNumeros(0,100));
+
+
+//Ejercicio 5
+
+//https://donnierock.com/2011/11/05/validar-un-dni-con-javascript/
+function nif(dni) {
+    let numero;
+    let letr;
+    let letra;
+    let expresion_regular_dni;
+   
+    expresion_regular_dni = /^\d{8}[a-zA-Z]$/;
+   
+    if(expresion_regular_dni.test (dni) == true){
+       numero = dni.substr(0,dni.length-1);
+       letr = dni.substr(dni.length-1,1);
+       numero = numero % 23;
+       letra='TRWAGMYFPDXBNJZSQVHLCKET';
+       letra=letra.substring(numero,numero+1);
+      if (letra!=letr.toUpperCase()) {
+         alert('Dni erroneo, la letra del NIF no se corresponde');
+       }else{
+         alert('Dni correcto');
+       }
+    }else{
+       alert('Dni erroneo, formato no válido');
+     }
+  }
+
+  //nif("44667260H");
+
+  //Ejercicio 6
+
+  function invertir(cadena){
+      let cadenaInvertida = "";
+      for(let i = cadena.length-1; i >= 0; i--){
+          cadenaInvertida += cadena.charAt(i);
+      }
+      return cadenaInvertida;
+  }
+  console.log(invertir(invertir("pepe")))
+
+  function esPalindromo(cadena){
+      return (cadena === invertir(cadena));        
+  }
+
+  console.log(esPalindromo("ama"));
