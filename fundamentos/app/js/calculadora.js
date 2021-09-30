a6683b729d42abfde31c054544fbc2aa84418692
blmanu <banderalazaromanuel@gmail.com>
2021-09-30 17:57:17 +0200

    calculadora 1

--- fundamentos/app/js/calculadora.js
+++ fundamentos/app/js/calculadora.js
@@ -1,5 +1,67 @@
-class calculadora{
-    
+
+class Calculadora {
+    constructor() {
+        this.NumeroPantalla = '0';
+        this.Op = '+';
+        this.Resultado = 0;
+    }
+
+
+    PonerDigito(numero) {
+        if (this.NumeroPantalla === '0')
+            this.NumeroPantalla = numero;
+        else {
+            this.NumeroPantalla += numero;
+        }
+    }
+
+    Calcular(operador) {
+
+        let valor = parseFloat(this.NumeroPantalla);
+        let rslt = parseFloat(this.Resultado);
+
+        switch (this.Op) {
+            case '+':
+                rslt += valor;
+                break;
+            case '-':
+                rslt -= valor;
+                break;
+            case '*':
+                rslt *= valor;
+                break;
+            case '/':
+                rslt /= valor;
+                break;
+        }
+        this.Op = operador;
+        this.Resultado = rslt.toString();
+        this.NumeroPantalla = '0';
+    }
+
+    limpiarPantalla() {
+        this.NumeroPantalla = '0';
+    }
+
+    limpiarTodo() {
+        this.NumeroPantalla = '0';
+        this.Resultado = '0';
+        this.operador = '+'
+    }
+
+    borrar() {
+        if (this.NumeroPantalla.length > 1 && this.NumeroPantalla != '0')
+            this.NumeroPantalla = this.NumeroPantalla.substring(0, this.NumeroPantalla.length - 1);
+        else
+            this.NumeroPantalla = 0;
+    }
+
+}
+
+function calc() {
+    let c = new Calculadora();
+    let resultado = document.querySelector("#resultado");
+    let pantalla = document.querySelector("#pantalla");
     let btCE = document.querySelector("#btCE");
     let btC = document.querySelector("#btC");
     let btBorrar = document.querySelector("#btBorrar");
@@ -21,5 +83,59 @@ class calculadora{
     let btComa = document.querySelector("#btComa");
     let btIgual = document.querySelector("#btIgual");
 
+    let actualizarPantalla = () => {
+        resultado.textContent = c.Resultado;
+        pantalla.textContent = c.NumeroPantalla;
+    }
 
+    let clicarNumero = (ev) => {
+        c.PonerDigito(ev.target.value);
+        actualizarPantalla();
+    }
+
+    let clicarOperacion = (ev) => {
+        c.Calcular(ev.target.value);
+        actualizarPantalla();
+    }
+
+
+    bt0.addEventListener('click', clicarNumero);
+    bt1.addEventListener('click', clicarNumero);
+    bt2.addEventListener('click', clicarNumero);
+    bt3.addEventListener('click', clicarNumero);
+    bt4.addEventListener('click', clicarNumero);
+    bt5.addEventListener('click', clicarNumero);
+    bt6.addEventListener('click', clicarNumero);
+    bt7.addEventListener('click', clicarNumero);
+    bt8.addEventListener('click', clicarNumero);
+    bt9.addEventListener('click', clicarNumero);
+    btComa.addEventListener('click', (ev) => {
+        if (c.NumeroPantalla.includes(','))
+            return;
+        else
+            clicarNumero(ev);
+
+    });
+
+    btCE.addEventListener('click', () => {
+        c.limpiarTodo();
+        actualizarPantalla();
+    })
+    btC.addEventListener('click', () => {
+        c.limpiarPantalla();
+        actualizarPantalla();
+    })
+    btBorrar.addEventListener('click', () => {
+        c.borrar();
+        actualizarPantalla();
+    })
+
+    btSumar.addEventListener('click', clicarOperacion);
+    btRestar.addEventListener('click', clicarOperacion);
+    btMultiplicar.addEventListener('click', clicarOperacion);
+    btDividir.addEventListener('click', clicarOperacion);
+    btIgual.addEventListener('click', clicarOperacion);
 }
+
+
+calc();
--- fundamentos/spec/calculadora.spec.js
+++ fundamentos/spec/calculadora.spec.js
@@ -1,9 +1,31 @@
 fdescribe('calculadora', () => {
-    it('escribir boton', ()=>{
+    it('ponerDigito', () => {
+        let c = new Calculadora();
+        c.PonerDigito('3');
+        expect(c.NumeroPantalla).toBe('3');
+        c.PonerDigito('5');
+        expect(c.NumeroPantalla).toBe('35');
+        c.PonerDigito('3');
+        expect(c.NumeroPantalla).toBe('353');
 
     })
-    
-    it('suma', () => {
+
+    it('calcular', () => {
+        let c = new Calculadora();
+        c.PonerDigito('3');
+        c.Calcular('-');
+        expect(c.Resultado).toBe('3');
+        c.PonerDigito('1');
+        c.Calcular('*');
+        expect(c.Resultado).toBe('2');
+        c.PonerDigito('1');
+        c.PonerDigito('0');
+        c.Calcular('/');
+        expect(c.Resultado).toBe('20');
+        c.PonerDigito('4');
+        c.Calcular('+');
+        expect(c.Resultado).toBe('5');
+        expect(c.Op).toBe('+');
 
     })
 })
