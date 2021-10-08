77938419ad13e5df6c772f602dd55bd626c19993
blmanu <banderalazaromanuel@gmail.com>
2021-10-08 15:23:49 +0200

    antes de empezar los ejercicios de validaciones

---
+++ angular/src/lib/my-core/directives/my-show-errors.directive.spec.ts
@@ -0,0 +1,8 @@
+import { MyShowErrorsDirective } from './my-show-errors.directive';
+
+describe('MyShowErrorsDirective', () => {
+  it('should create an instance', () => {
+    const directive = new MyShowErrorsDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- angular/src/lib/my-core/my-core.module.ts
+++ angular/src/lib/my-core/my-core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SizerComponent } from './components/sizer.component';
 import { PIPES_CADENAS } from './pipes/cadenas.pipe';
+import { MyShowErrorsDirective } from './directives/my-show-errors.directive';
 
 
 
@@ -9,6 +10,7 @@ import { PIPES_CADENAS } from './pipes/cadenas.pipe';
   declarations: [
     SizerComponent,
     PIPES_CADENAS,
+    MyShowErrorsDirective,
   ],
   exports: [
     SizerComponent, PIPES_CADENAS,
