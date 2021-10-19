5f18e014cb84d742e6e559f6dfa541616a05070a
blmanu <banderalazaromanuel@gmail.com>
2021-10-19 10:47:27 +0200

    Validador dni java

---
+++ microservicios/Pruebas/src/com/examples/Validadores.java
@@ -0,0 +1,17 @@
+package com.examples;
+
+import java.util.regex.Pattern;
+
+public class Validadores {
+	
+	public boolean isNIF(String dni){
+		Pattern patron = Pattern.compile("[0-9]{7,8}[A-Z a-z]");
+		if (!patron.matcher(dni).matches()) {
+			return false;			
+		}
+	    String letterValue = dni.substring(dni.length() - 1);
+	    String numberValue = dni.substring(0, dni.length() - 1);
+	    return Character.toUpperCase(letterValue.charAt(0)) == 
+	    		"TRWAGMYFPDXBNJZSQVHLCKE".charAt(Integer.parseInt(numberValue) % 23);
+	}
+}
---
+++ microservicios/Pruebas/src/com/examples/ValidadoresTest.java
@@ -0,0 +1,47 @@
+package com.examples;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class ValidadoresTest {
+	Validadores val;
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		val = new Validadores();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	
+	@ParameterizedTest
+	@DisplayName("DNIs no validos")
+	@CsvSource({ "4444", "hh", "12345678A", "''", "@NullString" })
+	void testNifErroneo(String dni) {
+		assertFalse(val.isNIF(dni));
+	}
+	
+	@ParameterizedTest
+	@DisplayName("DNIs validos")
+	@CsvSource({ "44667260H", "15500380J", "28714764T"})
+	void testNifBueno(String dni) {
+		assertTrue(val.isNIF(dni));
+	}	
+
+}
