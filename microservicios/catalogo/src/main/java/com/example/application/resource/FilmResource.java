302770ae0135b4452d9f36bff6d48e8c5488b6be
blmanu <banderalazaromanuel@gmail.com>
2021-10-25 17:15:23 +0200

    Antes del FilmResource

--- microservicios/catalogo/src/main/java/com/example/application/resource/FilmResource.java
+++ microservicios/catalogo/src/main/java/com/example/application/resource/FilmResource.java
@@ -1,5 +1,16 @@
 package com.example.application.resource;
 
-public class FilmResource {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.domains.contracts.services.FilmService;
+
+@RestController
+@RequestMapping(path = "/peliculas")
+public class FilmResource {
+	@Autowired
+	FilmService srv;
+	
+	
 }
